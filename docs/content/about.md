1f8e4c82f6cab1b47e63cc64d1b070909622b87a
Jona Ittermann <jona-ittermann@web.de>
2021-09-29 23:46:51 +0200

    Created about.vue. Added netlify form

--- docs/content/about.md
+++
@@ -1,9 +0,0 @@
----
-title: About
-description: The about page
----
-
-## Contact
-Jona Ittermann (jitter2s)
-
-jona.ittermann@smail.inf.h-brs.de
---
+++ docs/pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>
+      Jona Ittermann @ HBRS
+    </p>
+
+    <h3 class="mt-5">Contact</h3>
+    <form name="contact" netfliy>
+      <b-form-group label="Name" description="Enter your name.">
+        <b-form-input name="name" type="text" required />
+      </b-form-group>
+
+      <b-form-group label="Email" description="Enter your email address.">
+        <b-form-input name="email" type="email" required />
+      </b-form-group>
+
+      <b-form-group label="Your message" description="Let me know what you think!">
+        <b-form-textarea name="message" required />
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Send!</b-button>
+    </form>
+  </div>
+</template>
