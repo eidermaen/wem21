7d6fc14a5c76e6fc73abcd9a66e9a5a9fd78cfdf
Jona Ittermann <jona-ittermann@web.de>
2021-09-30 14:53:06 +0200

    Minor form update

--- docs/pages/about.vue
+++ docs/pages/about.vue
@@ -6,7 +6,7 @@
     </p>
 
     <h3 class="mt-5">Contact</h3>
-    <form name="wem_contact" action="POST" data-netlify="true">
+    <form name="wem_contact" data-netlify="true">
       <b-form-group label="Name" description="Enter your name.">
         <b-form-input name="name" type="text" required />
       </b-form-group>
