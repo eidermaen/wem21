7ffc8f64739c8c5d38ee47f93f02d41a04d0f477
Jona Ittermann <jona-ittermann@web.de>
2021-10-03 13:57:58 +0200

    Added generate object to nuxt.config.js

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -45,6 +45,15 @@ export default {
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content');
+      const files = await $content({ deep: true }).only(['path']).fetch();
+
+      return files.map(file => file.path === '/index' ? '/' : file.path);
+    }
+  },
+
   storybook: {
     stories: [
       '~/stories/**/*.stories.js',
