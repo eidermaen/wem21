0f3a2b8b4cbc3bca144aea0b1e21e0c8b7538355
Jona Ittermann <jona-ittermann@web.de>
2021-12-14 20:22:48 +0100

    Blatt 10: Added exercises

---
+++ übungen/blatt10/01_elements/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <h1>Vue components</h1>
+
+    <div class="content">
+      <CustomMenu class="top-nav">
+        <MenuItem to="start">
+          Start
+        </MenuItem>
+        <MenuItem to="about">
+          About
+        </MenuItem>
+      </CustomMenu>
+
+      <CustomMenu vertical>
+        <MenuItem to="test">
+          Test
+        </MenuItem>
+        <MenuItem to="another">
+          Another entry
+        </MenuItem>
+      </CustomMenu>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import CustomMenu from './components/CustomMenu.vue';
+import MenuItem from './components/MenuItem.vue';</script>
+
+<style scoped>
+.container {
+  max-width: 75%;
+  margin: 2rem auto;
+}
+
+.content {
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
+
+.top-nav {
+  grid-column: span 2;
+  margin-bottom: 2rem;
+}
+</style>
---
+++ übungen/blatt10/01_elements/src/components/MenuItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <a :href="to">
+    <slot />
+  </a>
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+
+const props = defineProps({
+  to: {
+    type: String,
+    required: true
+  }
+});
+
+</script>
+
+<style scoped>
+a {
+  margin: .5rem;
+  padding: 1rem 1.5rem;
+  border: none;
+  border-radius: .6rem;
+  background-color: orange;
+  transition: all 300ms;
+  cursor: pointer;
+  text-decoration: none;
+}
+
+a:hover {
+  background-color: lightseagreen;
+}
+</style>
