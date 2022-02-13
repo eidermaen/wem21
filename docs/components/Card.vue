8bae092e8b27bb51ef48ee776099ccde61256dde
Jona Ittermann <jona-ittermann@web.de>
2022-02-13 22:41:33 +0100

    Added shadow to Card component. rounded-lg on Toast

--- docs/components/Card.vue
+++ docs/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-40 p-4 m-2 border rounded-lg border-green-600 bg-green-400
-  hover:bg-green-300 text-white cursor-pointer transition-all duration-300">
+  hover:bg-green-300 text-white cursor-pointer transition-all duration-300 shadow-lg">
     <h3 class="text-xl font-bold">
       {{ title }}
     </h3>
