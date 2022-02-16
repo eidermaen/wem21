48ff442fc989a4a724587c1debb556d87f931da4
Jona Ittermann <jona-ittermann@web.de>
2022-02-16 12:32:56 +0100

    Ex.10: Added hint to 10.vue

--- docs/pages/exercises/10.vue
+++ docs/pages/exercises/10.vue
@@ -2,6 +2,8 @@
   <ExercisePage :exercise-id="10">
     <Info title="Info" variant="info">
       Da die Seite mit NuxtJS erstellt wurde, besteht die komplette UI aus VueJS Komponenten.
+      <br>
+      Die erstellen Vue-Komponenten für Übung 10 können im Storybook gefunden werden.
     </Info>
   </ExercisePage>
 </template>
